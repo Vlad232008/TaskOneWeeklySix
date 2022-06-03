1cd15060b7cefa91a8ea359a1d36e2bbd9f4a5f9
Rogachev_vv <vlad2320082009@gmail.com>
2022-06-03 14:31:12 +0400

    Готово

--- app/src/main/java/com/example/taskoneweeklysix/fragment/FragmentOne.kt
+++ app/src/main/java/com/example/taskoneweeklysix/fragment/FragmentOne.kt
@@ -1,7 +1,9 @@
 package com.example.taskoneweeklysix.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
+import android.os.Message
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,10 +14,14 @@ import java.math.BigDecimal
 
 class FragmentOne : BaseFragment() {
     private var const = BigDecimal(4)
-    private var x:Double = 0.0
-    private var y = BigDecimal(3)
+    private var Counter: Double = 0.0
+    private var result = BigDecimal(3)
+    private var show = ""
+    private var formula: Double = 0.0
+    private var count = 0
+    private var y = BigDecimal(4)
+    private var x = 1
     private var z = ""
-    private var v:Double = 0.0
     private lateinit var binding: FragmentOneBinding
     override fun onClickNew() {
     }
@@ -29,23 +35,56 @@ class FragmentOne : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentOneBinding.inflate(inflater, container, false)
-        Thread{
-            while (true) {
-                x += 1
-                if (x%2 == 1.0) {
-                    y += (const.divide(BigDecimal(x*2*(x*2+1)*(x*2+2)),300,0))
-                    z = y.toString()
-                } else {
-                    v = x*2*(x*2+1)*(x*2+2)
-                    y -= (const.divide(BigDecimal(x*2*(x*2+1)*(x*2+2)),300,0))
-                    z = y.toString()
+        val runnable = Runnable {
+            Thread {
+                while (true) {
+                    Counter += 1
+                    if (Counter % 2 == 1.0) {
+                        formula = Counter * 2 * (Counter * 2 + 1) * (Counter * 2 + 2)
+                        result += (const.divide(BigDecimal(formula), 500, 0))
+                        show = result.toString()
+                    } else {
+                        formula = Counter * 2 * (Counter * 2 + 1) * (Counter * 2 + 2)
+                        result -= (const.divide(BigDecimal(formula), 500, 0))
+                        show = result.toString()
+                    }
+                    if (count%2 ==0) {
+                        y -= (BigDecimal(4).divide(BigDecimal(x+2),500,0))
+                        z = y.toString()
+                        x += 2
+                    } else {
+                        y += (BigDecimal(4).divide(BigDecimal(x+2),500,0))
+                        z = y.toString()
+                        x += 2
+                    }
+                    if (Counter % 10000 == 0.0) {
+                        val msg = handler.obtainMessage()
+                        msg.obj = show
+                        handler.sendMessage(msg)
+                        val msg1 = handler1.obtainMessage()
+                        msg1.obj = z
+                        handler1.sendMessage(msg1)
+                    }
+                    count++
                 }
-                if (x % 10000 == 0.0) {
-                    binding.tvOne.text = z
-                }
-            }
-        }.start()
+            }.start()
+        }
+        val thread = Thread(runnable)
+        thread.start()
         return binding.root
+    }
+
+    private var handler: Handler = @SuppressLint("HandlerLeak")
+    object : Handler() {
+        override fun handleMessage(msg: Message) {
+            binding.tvOne.text = msg.obj.toString()
+        }
+    }
+    private var handler1: Handler = @SuppressLint("HandlerLeak")
+    object : Handler() {
+        override fun handleMessage(msg: Message) {
+            binding.tvTwo.text = msg.obj.toString()
+        }
     }
 
     companion object {
